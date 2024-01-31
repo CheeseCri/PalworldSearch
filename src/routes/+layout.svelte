907a37618883bfeda366a087eda262442f00af1b
CheeseCri <yunghn3@gmail.com>
2024-01-31 16:38:58 +0900

    페이지 구성
    - layout 페이지 구성
    - layout 이미지 추가
    - input, search 페이지 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,29 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+    import { page } from "$app/stores";
+    import "../app.pcss";
+    import logo from "$lib/assets/Palworld-logo.svg";
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+    } from "flowbite-svelte";
+    $: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+    <NavBrand href="/">
+        <img src={logo} class="me-3 h-6 sm:h-9" alt="Palworld Logo" />
+        <span
+            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+            >Palworld 도감 검색</span
+        >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/input">About</NavLi>
+        <NavLi href="/search">Navbar</NavLi>
+    </NavUl>
+</Navbar>
